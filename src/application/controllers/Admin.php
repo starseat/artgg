63e31230f5afa1dd54d9993937c8844a22325d86
starseat <starseat3@gmail.com>
2021-08-04 08:50:23 +0900

    [web] admin 에 대한 controller 처리 추가 (_admin 에 대해서 생각필요?)

---
+++ src/application/controllers/Admin.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends Base_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('session');
+	}
+
+	public function index()
+	{
+		log_message('artgg', '[admin] enter..');
+		$this->load->helper('url');
+		if ($this->session->userdata('is_login')) {
+			// todo..
+			log_message('artgg', '[admin] login ok');
+		}
+		else {
+			log_message('artgg', '[admin] is not login. redirect [/_admin/auth/login]');
+			redirect('/_admin/auth/login');
+		}
+		
+	}
+}
---
+++ src/application/controllers/_admin/Auth.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+include_once(APPPATH . 'controllers/_admin/AdminController.php');
+
+class Auth extends AdminController {
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index() {
+		log_message('artgg', '[auth] enter..');
+		if ($this->session->userdata('is_login')) {
+			log_message('artgg', '[auth] login ok');
+		}
+		else {
+			log_message('artgg', '[auth] is not login.. go to login()');
+			$this->login();
+		}
+	}
+
+	public function login() {
+		log_message('artgg', '[auth.login] enter..');
+		$this->load->view('admin/login');
+	}
+
+	public function logout() {
+		// todo..
+	}
+}
