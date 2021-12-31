491f8ad083cc381e24edc24749ebf53b406c83b3
starseat <starseat3@gmail.com>
2021-12-31 13:20:55 +0900

    아트경기 소개 유투브 링크 수정

--- src/index.php
+++ src/index.php
@@ -161,7 +161,8 @@
                                 </a>
                             </div>
                             <div class="introduce_player" id="target_youtube_box">
-                                <iframe id="target_youtube_view" width="510" height="300" src="https://www.youtube.com/embed/3riELB0MHYM?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <!-- <iframe id="target_youtube_view" width="510" height="300" src="https://www.youtube.com/embed/3riELB0MHYM?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                                <iframe id="target_youtube_view" width="510" height="300" src="https://www.youtube.com/embed/iU_LGwV5xPs?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                         </div>
                     </div>
