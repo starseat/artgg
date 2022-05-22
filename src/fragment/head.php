ba226e14502f0c1a8f0d778e1bf3a64551b4e285
jw.lee <starseat3@gmail.com>
2022-05-22 21:28:07 +0900

    title 중 2021 제거

--- src/fragment/head.php
+++ src/fragment/head.php
@@ -10,11 +10,11 @@ session_start();
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>2021 아트경기 | Art Gyeonggi</title>
+    <title>아트경기 | Art Gyeonggi</title>
     <!-- <meta name="description" content="24시간 365일, 오프라인에서 온라인까지, 경기도 작가의 미술품을 당신의 일상과 잇다."> -->
     <meta name="description" content="24시간 365일, 오프라인에서 온라인까지, 경기도 작가의 미술품을 당신의 일상과 잇다.">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="2021 아트경기 ">
+    <meta property="og:title" content="아트경기 ">
     <!-- <meta property="og:description" content="24시간 365일, 오프라인에서 온라인까지, 경기도 작가의 미술품을 당신의 일상과 잇다. "> -->
     <meta property="og:image" content="./img/meta-img.png" />
     <meta name="og:description" content="24시간 365일, 오프라인에서 온라인까지, 경기도 작가의 미술품을 당신의 일상과 잇다.">
